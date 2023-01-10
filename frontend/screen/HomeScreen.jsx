07b258a82105382db295d42793696acf17f5af6e
MeshachArinze <meshacharinze@gmail.com>
2023-01-10 13:43:47 +0100

    refactor to my Repo

--- frontend/screen/HomeScreen.jsx
+++ frontend/screen/HomeScreen.jsx
@@ -23,7 +23,52 @@ const HomeScreen = () => {
     error: errorSellers,
     user: sellers,
   } = userTopSellersList;
-  return <div>HomeScreen</div>;
+
+  useEffect(() => {
+    dispatch(listProducts({}));
+    dispatch(listTopSellers());
+
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h2>Top Sellers</h2>
+      {loadingSellers ? (
+        <LoadingBox></LoadingBox>
+      ) : errorSellers ? (
+        <MessageBox variant="danger">{errorSellers}</MessageBox>
+      ) : (
+        <>
+          {sellers.length === 0 && <MessageBox>No Seller Found</MessageBox>}
+          <Carousel showArrows autoPlay showThumbs={false}>
+            {sellers.map((seller) => (
+              <div key={seller._id}>
+                <Link to={`/seller/${seller._id}`}>
+                  <img src={seller.seller.logo} alt={seller.seller.name} />
+                  <p className="legend">{seller.seller.name}</p>
+                </Link>
+              </div>
+            ))}
+          </Carousel>
+        </>
+      )}
+      <h2>Featured Products</h2>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <>
+          {products.length === 0 && <MessageBox>No Product Found</MessageBox>}
+          <div className="row center">
+            {products.map((product) => (
+              <Product key={product._id} product={product}></Product>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default HomeScreen;

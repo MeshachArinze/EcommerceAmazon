705f8655694ca73a36eb3b3172f83d56e1a6944f
MeshachArinze <meshacharinze@gmail.com>
2023-01-10 13:48:19 +0100

    refactor to my Repo

---
+++ frontend/screen/MapScreen.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useState} from 'react';
+import { Load } from "";
+
+const MapScreen = () => {
+  return (
+    <div>MapScreen</div>
+  )
+}
+
+export default MapScreen

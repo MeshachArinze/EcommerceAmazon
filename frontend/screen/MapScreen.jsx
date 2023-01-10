0f1421e3fdf9cc3e8ee234382d58a386d80f98f3
MeshachArinze <meshacharinze@gmail.com>
2023-01-10 14:01:11 +0100

    refactor to my Repo

--- frontend/screen/MapScreen.jsx
+++ frontend/screen/MapScreen.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect, useState} from 'react';
-import { Load } from "";
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import LoadingBox from "../components/LoadingBox";
+import Axios from "axios";
+import { USER_ADDRESS_MAP_CONFIRM } from "../constants/userConstants";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 
 const MapScreen = () => {
   return (

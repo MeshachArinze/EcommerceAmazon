6371bdc32e33b663c17954d180ed150617227ef7
MeshachArinze <meshacharinze@gmail.com>
2023-01-10 13:40:55 +0100

    refactor to my Repo

--- frontend/screen/HomeScreen.jsx
+++ frontend/screen/HomeScreen.jsx
@@ -1,20 +1,29 @@
-import React, { useEffect  } from 'react';
+import React, { useEffect } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-import Product from '../components/Product';
-import LoadingBox from '../components/LoadingBox';
-import { useDispatch, useSelector } from '../node_modules/react-redux/es/exports';
-import { listProducts } from '../actions/productActions';
-import { listTopSellers } from '../actions/userActions';
-import { Link } from '../node_modules/react-router-dom/dist/index';
-
+import Product from "../components/Product";
+import LoadingBox from "../components/LoadingBox";
+import {
+  useDispatch,
+  useSelector,
+} from "../node_modules/react-redux/es/exports";
+import { listProducts } from "../actions/productActions";
+import { listTopSellers } from "../actions/userActions";
+import { Link } from "../node_modules/react-router-dom/dist/index";
 
 const HomeScreen = () => {
-    const dispatch = useDispatch();
-    const 
-  return (
-    <div>HomeScreen</div>
-  )
-}
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, product } = productList;
 
-export default HomeScreen
+  const userTopSellersList = useSelector((state) => state.userTopSellersList);
+
+  const {
+    loading: loadingSellers,
+    error: errorSellers,
+    user: sellers,
+  } = userTopSellersList;
+  return <div>HomeScreen</div>;
+};
+
+export default HomeScreen;
